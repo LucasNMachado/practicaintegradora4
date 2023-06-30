e642d7692ad10d5f1890736a0b23dcb5fc431d27
Lucas <machadolucasn@gmail.com>
2023-06-29 23:30:32 -0300

    .

--- src/app.js
+++ src/app.js
@@ -2,32 +2,37 @@ const express = require('express');
 const productsRoutes = require('./routes/productsRoutes');
 const cartsRoutes = require('./routes/cartsRoutes');
 const exphbs = require('express-handlebars');
-
- 
+const path = require('path');
+const mongoose = require('mongoose');
 const http = require('http');
 const socketIO = require('socket.io');
 
 const app = express();
 const port = 8080;
 
-
 app.engine('handlebars', exphbs.engine);
-
-
-
 app.set('view engine', 'handlebars');
-
+app.set('views', path.join(__dirname, 'src', 'views'));
 app.use(express.json());
 
-
 const server = http.createServer(app);
-
-
 const io = socketIO(server);
-
 
 io.on('connection', (socket) => {
   console.log('Cliente conectado');
+});
+
+
+const MONGO = 'mongodb+srv://machadolucasn:machadolucasn@cluster0.imos5vy.mongodb.net/';
+mongoose.connect(MONGO, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('MongoDB connetion error:', error);
+  process.exit(1); 
 });
 
 app.use('/api/products', productsRoutes);
@@ -36,4 +41,3 @@ app.use('/api/carts', cartsRoutes);
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
--- src/dao/productManager.js
+++ src/dao/productManager.js
@@ -37,3 +37,4 @@ class ProductManager {
 }
 
 module.exports = ProductManager;
+ 
