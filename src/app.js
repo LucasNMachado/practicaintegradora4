4ee394300690eaaf590f6aa79b5b9c8af921e961
Lucas <machadolucasn@gmail.com>
2023-07-20 17:33:54 -0300

    desafio

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ const socketIO = require("socket.io");
 const homeRouter = require("./routes/homeRoutes");
 const ProductManager = require("../src/dao/mongo/productManager");
 const MessageManager = require("../src/dao/mongo/messageManager");
+import MongoStore from 'connect-mongo';
+import viewsRoutes from './routes/viewsRoutes'
+import sessionsRoutes from './routes/sessionsRoutes'
 
 const app = express();
 const port = 8080;
@@ -22,6 +25,8 @@ app.use("/public", express.static(path.join(__dirname, "../public")));
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 app.use("/", homeRouter);
+app.use("/", viewsRoutes);
+app.use("/api/sessions", sessionsRoutes);
 
 const server = http.createServer(app);
 const io = socketIO(server);
@@ -68,3 +73,31 @@ mongoose
     console.error("MongoDB connection error:", error);
     process.exit(1);
   });
+
+// conexión a session de mongo
+
+  app.use(session({
+    secret:'coderhouse',
+    resave: true,
+    saveUninitialized: true,
+    store: MongoStore.create({
+    mongoUrl: 'mongodb+srv://machadolucasn:machadolucasn@cluster0.imos5vy.mongodb.net/sessions_mongo?retryWrites=true&w=majority',
+    ttl:100, 
+    mongoOptions: {useNewUrlParser: true, useUnifiedTopology: true}, 
+    })
+ 
+}));
+
+
+app.get('/session', (req, res) => {
+    if (!req.session.count) {
+        req.session.count = 1;
+        res.send('Bienvenido a la pagina');
+        return;
+    }
+
+    req.session.count++;
+    res.send(`Usted ha visitado la pagina ${req.session.count} veces`);
+})
+
+
